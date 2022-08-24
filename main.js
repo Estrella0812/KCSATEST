508fb4c31d2ece36bcadc4c351188a88592fd1a6
Estrella0812 <88253146+Estrella0812@users.noreply.github.com>
2022-08-24 19:48:24 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,88 @@
+var slideIndex = 0;
+var countdown = 0;
+showSlides();
+
+function currentSlide(n) {
+    slideIndex=n-1;
+    clearTimeout(countdown);
+    showSlides();
+}
+
+function showSlides() {
+    var i;
+    slides = document.getElementsByClassName("mySlides");
+    dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    countdown = setTimeout(showSlides, 5000);
+}
+
+
+
+
+
+window.onload = function  onLoad(){
+    document.getElementById("club_stat").setAttribute("src", "src/"  + "over_age_40_club_stat.jpg");
+    document.getElementById("player_stat").setAttribute("src", "src/" + "over_age_40_player_stat.jpg");
+    
+    document.getElementById("over_age_40_club_stat").style.color = "black";
+    document.getElementById("over_age_40_player_stat").style.color = "black";
+    document.getElementById("open_age_club_stat").style.color = "grey";
+    document.getElementById("open_age_player_stat").style.color = "grey";
+}
+
+
+function clubStat(n){
+    setClubButtonGrey();
+    document.getElementById("club_stat").setAttribute("src", "src/" + n + ".jpg");
+    document.getElementById(n).style.color = "black";
+    //console.log(n);
+}
+
+function playerStat(n){
+    setPlayerButtonGrey();
+    document.getElementById("player_stat").setAttribute("src", "src/" + n + ".jpg");
+    document.getElementById(n).style.color = "black";
+}
+
+function setClubButtonGrey(){
+    document.getElementById("open_age_club_stat").style.color = "grey";
+    document.getElementById("over_age_40_club_stat").style.color = "grey";
+    document.getElementById("over_age_55_club_stat").style.color = "grey";
+}
+
+function setPlayerButtonGrey(){
+    document.getElementById("open_age_player_stat").style.color = "grey";
+    document.getElementById("over_age_40_player_stat").style.color = "grey";
+    document.getElementById("over_age_55_player_stat").style.color = "grey";
+}
+
+
+
+
+
+
+
+
+//TOGGLE MENU
+var menuList = document.getElementById("menuList");
+menuList.style.maxHeight = "0px";
+
+function toggleMenu(){
+    if(menuList.style.maxHeight=="0px"){
+        menuList.style.maxHeight = "300px";
+    }
+    else if(menuList.style.maxHeight=="300px"){
+        menuList.style.maxHeight = "0px";
+    }
+    console.log("helloWYEASDQWA")
+}
---
+++ sub-main.js
@@ -0,0 +1,13 @@
+//TOGGLE MENU
+var menuList = document.getElementById("menuList");
+menuList.style.maxHeight = "0px";
+
+function toggleMenu(){
+    if(menuList.style.maxHeight=="0px"){
+        menuList.style.maxHeight = "300px";
+    }
+    else if(menuList.style.maxHeight=="300px"){
+        menuList.style.maxHeight = "0px";
+    }
+    console.log("helloWYEASDQWA")
+}
